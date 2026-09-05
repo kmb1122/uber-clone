2b998a487a0a1fefb838be89fe33cdc0b6a0422d
kmb1122 <silverspace1850@gmail.com>
2026-09-05 11:01:17 -0400

    Assets Added

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,12 @@
-import { StatusBar } from "expo-status-bar";
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function HomeScreen() {
+const Home = () => {
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-white">
-      <Text className="text-red-500">Uber Clone</Text>
-      <StatusBar style="auto" />
+    <SafeAreaView>
+      <Text>Home</Text>
     </SafeAreaView>
   );
-}
+};
+
+export default Home;
