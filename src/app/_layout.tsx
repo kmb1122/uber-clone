87b86c0e98a080444508853ceabfd5dcee970f2b
kmb1122 <silverspace1850@gmail.com>
2026-09-06 09:03:46 -0400

    Added Fonts

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,21 +1,27 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [loaded] = useFonts({
+    "Jakarta-Bold": require("../../assets/fonts/PlusJakartaSans-Bold.ttf"),
+    "Jakarta-ExtraBold": require("../../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+    "Jakarta-ExtraLight": require("../../assets/fonts/PlusJakartaSans-ExtraLight.ttf"),
+    "Jakarta-Light": require("../../assets/fonts/PlusJakartaSans-Light.ttf"),
+    "Jakarta-Medium": require("../../assets/fonts/PlusJakartaSans-Medium.ttf"),
+    "Jakarta-Regular": require("../../assets/fonts/PlusJakartaSans-Regular.ttf"),
+    "Jakarta-SemiBold": require("../../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+  });
+
+  if (!loaded) return null;
+
+  SplashScreen.hideAsync();
+
   return (
     <Stack>
-      <Stack.Screen
-        name="index"
-        options={{
-          headerShown: false,
-        }}
-      />
-
-      <Stack.Screen
-        name="+not-found"
-        options={{
-          title: "Not Found",
-        }}
-      />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="+not-found" options={{ title: "Not Found" }} />
     </Stack>
   );
 }
