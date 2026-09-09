e967e4fed811f38761bd3b948809ff3bb645a0a7
kmb1122 <silverspace1850@gmail.com>
2026-09-09 11:33:31 -0400

    Variant Styles

--- src/app/(auth)/welcome.tsx
+++ src/app/(auth)/welcome.tsx
@@ -1,7 +1,7 @@
 import { onboarding } from "@/constants";
 import { router } from "expo-router";
 import { useRef, useState } from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Swiper from "react-native-swiper";
 
@@ -32,8 +32,20 @@ const Onboarding = () => {
         onIndexChanged={(index) => setActiveIndex(index)}
       >
         {onboarding.map((item) => (
-          <View key={item.id}>
-            <Text>{item.title}</Text>
+          <View className="flex items-center justify-center p-5" key={item.id}>
+            <Image
+              source={item.image}
+              className="w-full h-[300px]"
+              resizeMode="contain"
+            />
+            <View className="flex flex-row items-center justify-ccenter w-full mt-10">
+              <Text className="text-black text-3xl font-bold mx-10 text-center">
+                {item.title}
+              </Text>
+              <Text className="text-lg font-JakartaSemiBold text-center text-[#858585] mx-10 mt-3">
+                {item.description}
+              </Text>
+            </View>
           </View>
         ))}
       </Swiper>
